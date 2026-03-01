4acaaa586454eb11ce0eea078f3e7744690d5d2d
Kelly Nie <kellynie0521@ucla.edu>
2026-03-01 10:17:05 -0800

    Initial Commit

---
+++ HOTH Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HOTH Project
+//
+//  Created by 聂楚涵 on 3/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HOTH Project/HOTH_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HOTH_ProjectApp.swift
+//  HOTH Project
+//
+//  Created by 聂楚涵 on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct HOTH_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
